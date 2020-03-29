83e141d454871cbc32e37fa8a8ee4f2188c1a112
Sun <Sun@DESKTOP-F5BJNOR>
2020-03-29 16:07:19 +0900

    ok

---
+++ src/main/java/com/dingpet/customers/P001/controller/P001Controller.java
@@ -0,0 +1,25 @@
+package com.dingpet.customers.P001.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/customers/p001")
+@Controller
+public class P001Controller {
+	
+	@RequestMapping("/signin")
+	public void signIn() {
+		
+	}
+	
+	@RequestMapping("/signup")
+	public void signUp() {
+		
+	}
+	
+	@RequestMapping("/change")
+	public void change() {
+		
+	}
+	
+}
